a80a6afc27876f1f5a0a34373c88c45bbef97604
bus42 <gbrewton1980@gmail.com>
2023-02-14 16:32:48 -0700

    Corrects file naming, reduces NPC boost

--- Scripts/npcboost.bat
+++
@@ -1,8 +0,0 @@
-@REM This is a simple script that I wrote because vanilla NPCs have ridiculously low stats and that sucks for Sim Settlements 2
-modav strength 4
-modav perception 4
-modav endurance 4
-modav charisma 4
-modav intelligence 4
-modav agility 4
-modav luck 4
